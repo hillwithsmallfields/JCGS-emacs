57406af27df80e6f406bee417df8bde4ba13c907
John Sturdy <johstu01@e104804-lin.cambridge.arm.com>
2014-07-07 11:42:54 +0100

    new way of finding my emacs directory tree

--- basics/jcgs-common-setup.el
+++ basics/jcgs-common-setup.el
@@ -1,18 +1,27 @@
 ;;;; JCGS's common emacs setup
-;;; Time-stamp: <2014-06-20 16:16:01 johstu01>
+;;; Time-stamp: <2014-07-01 10:57:41 johstu01>
 ;;;
 ;;; Things I want even in most of my specialized emacsen
 
 (global-font-lock-mode 1)
 (setq inhibit-startup-screen t
       kill-whole-line t
-      display-time-day-and-date t
-      user-emacs-directory
-      (cond ((file-directory-p (substitute-in-file-name "$HOME/JCGS-emacs"))
-	     (substitute-in-file-name "$HOME/JCGS-emacs"))
-	    ((getenv "DROPBOX")
-	     (substitute-in-file-name "$DROPBOX/emacs"))
-	    (t (substitute-in-file-name "$HOME/Dropbox/emacs"))))
+      display-time-day-and-date t)
+
+(unless (and (stringp user-emacs-directory)
+	     (file-directory-p user-emacs-directory)
+	     (file-directory-p
+	      (expand-file-name "basics" user-emacs-directory)))
+  (setq user-emacs-directory
+	(catch 'found
+	  (dolist (raw-dir '("$HOME/JCGS-emacs" "/work/johstu01/JCGS-emacs"))
+	    (let ((dir (substitute-in-file-name raw-dir)))
+	      (when (and (stringp dir)
+			 (file-directory-p dir)
+			 (file-directory-p
+			  (expand-file-name "basics" dir)))
+		(throw 'found dir)))))))
+
 (display-time)
 (when (member (system-name)
 	      '("ezra"))
