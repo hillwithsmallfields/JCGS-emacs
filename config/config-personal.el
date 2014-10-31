b44f1adf4bd2ddae0fc4109716ff18a1a1a8c749
John Sturdy <jcg.sturdy@gmail.com>
2014-10-31 08:37:30 +0000

    Add flag for running manually, and change the directory.

--- config/config-personal.el
+++ config/config-personal.el
@@ -1,16 +1,17 @@
 ;;;; config-personal.el --- mount my personal journal etc
-;;; Time-stamp: <2013-12-10 22:49:03 jcgs>
+;;; Time-stamp: <2014-10-31 08:37:13 jcgs>
 
 (defun personal-setup (&optional manual)
-  "Set up my personal files volume."
+  "Set up my personal files volume.
+Optional argument MANUAL indicates running this manually."
   (interactive (list t))
-  (unless (file-directory-p "/mnt/crypted/personal")
+  (unless (file-directory-p "/mnt/crypted/jcgs/personal")
     (error "Personal files volume not mounted"))
-  (load-file "/mnt/crypted/personal/mount-drive.el")
+  (load-file "/mnt/crypted/jcgs/personal/mount-drive.el")
   (when manual
     (personal-drive-after-init-function)))
 
-(when (file-directory-p "/mnt/crypted/personal")
+(when (file-directory-p "/mnt/crypted/jcgs/personal")
   (personal-setup))
 
 ;;; config-personal.el ends here
