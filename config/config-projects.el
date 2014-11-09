05052c9b2449f026981043aa8ee0d943b52f8779
John Sturdy <jcg.sturdy@gmail.com>
2014-11-09 19:01:27 +0000

    Use function that substitutes environment variables and checks for directory presence.

--- config/config-projects.el
+++ config/config-projects.el
@@ -1,5 +1,5 @@
 ;;;; Configuration for project-specific things
-;;; Time-stamp: <2014-11-04 14:18:13 johstu01>
+;;; Time-stamp: <2014-11-09 19:00:51 jcgs>
 
 ;; Copyright (C) 2007, 2008, 2009, 2010, 2012, 2013, 2014, John C. G. Sturdy
 
@@ -21,48 +21,48 @@
 		  ("-ulga-" . "$OPEN_PROJECTS/ULGA/trunk/")
 		  ("-libGE-" . "$OPEN_PROJECTS/libGE/trunk/")))
     (make-named-shell (car pair) (cdr pair)))
-  ( "-rtl-" . "$OPEN_PROJECTS/libRTL/trunk/" )
+  ;; ( "-rtl-" . "$OPEN_PROJECTS/libRTL/trunk/" )
   (when nil
     (make-named-shell "-ephemerals-"
 		      "$COMMON/research/bds/grevo/ephemerals/"
 		      )))
 
-(make-named-shell "-muesli-"
-		  "$OPEN_PROJECTS/muesli/"
-		  ;; "make clean; ./configure; make\n"
-		  )
+(make-shell-for-directory-if-present "-muesli-"
+				     "$OPEN_PROJECTS/muesli/"
+				     ;; "make clean; ./configure; make\n"
+				     )
 
-(make-named-shell "-emacs-"
-		  "$EMACS")
+(make-shell-for-directory-if-present "-emacs-"
+				     "$EMACS")
 
 (when (at-work)
-  (make-named-shell "-src-"
-		    "/work/johstu01/build/trunk/work/src")
-  (make-named-shell "-monit-"
-		    "/work/johstu01/build/trunk/work/src/arm.com/uniSched/overseer/")
-  (make-named-shell "-dripfeed-"
-		    "/work/johstu01/build/trunk/work/src/arm.com/uniSched/dryrun"))
+  (make-shell-for-directory-if-present "-src-"
+				       "/work/johstu01/build/trunk/work/src")
+  (make-shell-for-directory-if-present "-monit-"
+				       "/work/johstu01/build/trunk/work/src/arm.com/uniSched/overseer/")
+  (make-shell-for-directory-if-present "-dripfeed-"
+				       "/work/johstu01/build/trunk/work/src/arm.com/uniSched/dryrun"))
 
 (unless (at-work)
-  (make-named-shell "-gos-"
-		    "$OPEN_PROJECTS/gos/"
-		    ;; "make clean; ./configure; make\n"
-		    )
-  (make-named-shell "-mulvoc-"
-		    "$OPEN_PROJECTS/mulvoc/mulvoc/"
-		    ;; "make clean; ./configure; make\n"
-		    )
+  (make-shell-for-directory-if-present "-gos-"
+				       "$OPEN_PROJECTS/gos/"
+				       ;; "make clean; ./configure; make\n"
+				       )
+  (make-shell-for-directory-if-present "-mulvoc-"
+				       "$OPEN_PROJECTS/mulvoc/mulvoc/"
+				       ;; "make clean; ./configure; make\n"
+				       )
   (when t
-    (make-named-shell "-khanate-"
-		      "$WRITING/fiction/last-khan/"
-		      ;; "latex last-khan.tex\n"
-		      ))
+    (make-shell-for-directory-if-present "-khanate-"
+					 "$WRITING/fiction/last-khan/"
+					 ;; "latex last-khan.tex\n"
+					 ))
   )
 
 (when (and nil (at-work))
   (let ((login-host (format "login%d.euhpc.arm.com" (1+ (random 7)))))
-    (make-named-shell (format "=%s=" login-host)
-		      "~"
-		      (format "echo ssh %s" login-host))))
+    (make-shell-for-directory-if-present (format "=%s=" login-host)
+					 "~"
+					 (format "echo ssh %s" login-host))))
 
 ;;; config-projects.el ends here
