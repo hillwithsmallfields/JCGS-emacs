f63632d7bdb169802ae4c568644b90858ec76f1a
John Sturdy <jcg.sturdy@gmail.com>
2015-03-15 22:58:22 +0000

    Further decoding of weather data.

--- information-management/metoffice.el
+++ information-management/metoffice.el
@@ -1,5 +1,5 @@
 ;;;; metoffice.el --- handle data from the UK's Meteorological Office
-;;; Time-stamp: <2015-03-15 22:31:24 jcgs>
+;;; Time-stamp: <2015-03-15 22:57:31 jcgs>
 
 (require 'json)
 
@@ -77,6 +77,50 @@ With optional DISPLAY-RESULT, show the result as a message."
 	  id)
       (error "Could not find a Met Office site called %s" name))))
 
+(defvar metoffice-weather-codes
+  '((0 . "Clear night")
+    (1 . "Sunny day")
+    (2 . "Partly cloudy (night)")
+    (3 . "Partly cloudy (day)")
+    (4 . "Not used")
+    (5 . "Mist")
+    (6 . "Fog")
+    (7 . "Cloudy")
+    (8 . "Overcast")
+    (9 . "Light rain shower (night)")
+    (10 . "Light rain shower (day)")
+    (11 . "Drizzle")
+    (12 . "Light rain")
+    (13 . "Heavy rain shower (night)")
+    (14 . "Heavy rain shower (day)")
+    (15 . "Heavy rain")
+    (16 . "Sleet shower (night)")
+    (17 . "Sleet shower (day)")
+    (18 . "Sleet")
+    (19 . "Hail shower (night)")
+    (20 . "Hail shower (day)")
+    (21 . "Hail")
+    (22 . "Light snow shower (night)")
+    (23 . "Light snow shower (day)")
+    (24 . "Light snow")
+    (25 . "Heavy snow shower (night)")
+    (26 . "Heavy snow shower (day)")
+    (27 . "Heavy snow")
+    (28 . "Thunder shower (night)")
+    (29 . "Thunder shower (day)")
+    (30 . "Thunder"))
+  "Weather codes from http://www.metoffice.gov.uk/datapoint/support/documentation/code-definitions")
+
+(defvar metoffice-visibility-codes-alist
+  '((UN . "Unknown")
+    (VP . "Very poor - Less than 1 km")
+    (PO . "Poor - Between 1-4 km")
+    (MO . "Moderate - Between 4-10 km")
+    (GO . "Good - Between 10-20 km")
+    (VG . "Very good - Between 20-40 km")
+    (EX . "Excellent - More than 40 km"))
+  "Visibility codes from http://www.metoffice.gov.uk/datapoint/support/documentation/code-definitions")
+
 ;;; following instructions from http://www.metoffice.gov.uk/datapoint/product/uk-daily-site-specific-forecast/detailed-documentation
 
 (defun metoffice-get-site-weather (&optional site)
@@ -120,14 +164,25 @@ days of forecast being given including today."
 			  (cons (intern (downcase (cdr (assoc '$ raw-array-entry))))
 				(mapcar (function
 					 (lambda (raw-pair)
-					   (cons
-					    (or (cdr (assoc (car raw-pair)
-							    parameter-descriptions))
-						(car raw-pair))
-					    (let ((data-string (cdr raw-pair)))
-					      (if (string-match "\\`[-0-9]+\\'" data-string)
-						  (string-to-number data-string)
-						data-string)))))
+					   (let ((data-name (or (cdr (assoc (car raw-pair)
+									    parameter-descriptions))
+								(car raw-pair)))
+						 (data-string (cdr raw-pair)))
+					     (if (eq data-name '$)
+						 (cons 'period (intern (downcase data-string)))
+					       (cons data-name
+						     (cond
+						      ((eq data-name 'weather-type)
+						       (or (cdr (assoc (string-to-number data-string)
+								       metoffice-weather-codes))
+							   data-string))
+						      ((eq data-name 'visibility)
+						       (or (cdr (assoc (intern data-string)
+								       metoffice-visibility-codes-alist))
+							   data-string))
+						      ((string-match "\\`[-0-9]+\\'" data-string)
+						       (string-to-number data-string))
+						      (t data-string)))))))
 					raw-array-entry))))
 		       (cdr (assoc 'Rep raw-data))))))
 	 forecast-data)))
@@ -135,8 +190,21 @@ days of forecast being given including today."
 (defun metoffice-get-site-period-weather (&optional site offset-days period)
   "Get the weather for SITE (or the default) for OFFSET-DAYS ahead at PERIOD.
 SITE defaults as for `metoffice-get-site-weather'.  OFFSET-DAYS is 0 by default,
-meaning today, and PERIOD can be 'day or 'night, the default being 'day."
-  (cdr (assoc (or period 'day)
+meaning today, and PERIOD can be 'day or 'night, the default being chosen
+according to the time this function was called at."
+  ;;       The documentation says "The value of the Rep object($ in
+  ;;       the JSON representation) denotes the number of minutes
+  ;;       after midnight UTC on the day represented by the Period
+  ;;       object in which the Rep object is found."
+  ;;       However, there's nothing in what json.el has made of it
+  ;;       that looks like a time in minutes, so I can't use that to
+  ;;       decide when day and night meet in terms of the data.
+  (cdr (assoc (or period
+		  (let ((hour (nth 2 (decode-time))))
+		    (if (and (>= hour 6) ; simple 12-hour split, in absence of sunrise/sunset data from ephemeris etc
+			     (<= hour 18))
+			'day
+		      'night)))
 	      (nth (or offset-days 0)
 		   (metoffice-get-site-weather site)))))
 
