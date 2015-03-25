cc3d49d31e67546b7da1c2774fdc9eb3de33836e
John Sturdy <jcg.sturdy@gmail.com>
2015-03-25 20:50:14 +0000

    Add standard comment

--- information-management/org-mode-pomodoros.el
+++ information-management/org-mode-pomodoros.el
@@ -1,5 +1,29 @@
 ;;;; Pomodoros
-;;; Time-stamp: <2015-03-16 21:31:13 jcgs>
+;;; Time-stamp: <2015-03-25 20:49:17 jcgs>
+
+;; Copyright (C) 2015  John Sturdy
+
+;; Author: John Sturdy <jcg.sturdy@gmail.com>
+;; Keywords: convenience, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (defvar jcgs/org-timer-pomodoros-done-count 0
   "Count of the pomodoros I have done.
