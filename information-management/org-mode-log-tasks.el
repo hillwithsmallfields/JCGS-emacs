591df2d3ac0ca25ff1d718cf8cc58d7d8de59b2c
John Sturdy <jcg.sturdy@gmail.com>
2015-03-25 20:49:59 +0000

    Add standard comment

--- information-management/org-mode-log-tasks.el
+++ information-management/org-mode-log-tasks.el
@@ -1,5 +1,29 @@
 ;;;; Write clocked-in tasks into my work log file
-;;; Time-stamp: <2015-03-16 21:33:06 jcgs>
+;;; Time-stamp: <2015-03-25 20:48:12 jcgs>
+
+;; Copyright (C) 2015  John Sturdy
+
+;; Author: John Sturdy <jcg.sturdy@gmail.com>
+;; Keywords: convenience, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (defvar jcgs/org-last-clocked-task-added-to-log nil
   "The last clocked task added to the log.")
