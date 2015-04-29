0379c8745a712d59648448582c8220d52fbd7a86
John Sturdy <jcg.sturdy@gmail.com>
2015-04-29 09:31:33 +0100

    Save agendas and prepare to push to mobile, on quit or in advance of planned shutdown.

--- special-setups/tasks/tasks-emacs-setup.el
+++ special-setups/tasks/tasks-emacs-setup.el
@@ -1,5 +1,5 @@
 ;;;; Emacs setup for task management only
-;;; Time-stamp: <2015-04-10 20:36:20 jcgs>
+;;; Time-stamp: <2015-04-29 09:30:58 jcgs>
 
 (setq debug-on-error t)
 
@@ -45,6 +45,30 @@
 (setq org-agenda-files (delete-if-not 'file-exists-p org-agenda-files))
 (org-mobile-pull)
 (org-agenda-list)
+
+(defun jcgs/emacs-pre-shutdown-function ()
+  "Function to run soon before shutdown."
+  (save-all-buffers-no-ask)
+  (org-mobile-push))
+
+(let ((shutdown-file "/tmp/shutdowntime"))
+  (when (file-readable-p shutdown-file)
+    (find-file shutdown-file)
+    (let* ((time-from-file (diary-entry-time (buffer-string)))
+	   (now (decode-time))
+	   (before-string (format-time-string 
+			   "%H%M"
+			   (time-subtract
+			    (encode-time 0 (% time-from-file 100) (/ time-from-file 100)
+					 (nth 3 now) (nth 4 now) (nth 5 now)
+					 (nth 8 now))
+			    (seconds-to-time 120)))))
+      (kill-buffer)
+      (message "Will prepare for shutdown at %s" before-string)
+      (run-at-time before-string nil 'jcgs/emacs-pre-shutdown-function)
+      )))
+
+(add-hook 'kill-emacs-hook 'jcgs/emacs-pre-shutdown-function)
 
 (setq debug-on-error nil)
 
