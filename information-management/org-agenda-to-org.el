181dd8980deda8c0a1c51fc03c5929ce41066708
John Sturdy <jcg.sturdy@gmail.com>
2016-03-11 17:06:53 +0000

    Remove org-mode enhancements from here as they're going into a repository of their own

--- information-management/org-agenda-to-org.el
+++
@@ -1,7 +0,0 @@
-;;;; Convert an org agenda into an org-mode buffer
-;;; Time-stamp: <2016-01-24 20:14:25 jcgs>
-
-(defun jcgs/org-agenda-to-org (source destination)
-  "Convert the agenda in SOURCE buffer to org in DESTINATION buffer."
-  (interactive "bConvert agenda buffer: \nBConvert %s into: \n")
-  )
