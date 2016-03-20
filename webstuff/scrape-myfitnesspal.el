c2f4500bc1965cc37839d975f100b807bfb14b33
John Sturdy <jcg.sturdy@gmail.com>
2016-03-20 20:52:52 +0000

    Get the column labels
    And some other bits.

--- webstuff/scrape-myfitnesspal.el
+++ webstuff/scrape-myfitnesspal.el
@@ -1,5 +1,5 @@
 ;;; Scrape data from myfitnesspal.com food diary pages
-;;; Time-stamp: <2016-03-19 20:50:10 jcgs>
+;;; Time-stamp: <2016-03-20 20:49:41 jcgs>
 ;;; The resulting data should convert easily to CSV etc.
 ;;; Started by John Sturdy <jcg.sturdy@gmail.com> on 2016-03-18
 
@@ -26,11 +26,24 @@
 	     (string-to-number (delete ?, str))))
 	  strings-list))
 
+(defun zip-cons-lists (a b)
+  (let ((result nil))
+    (while (and a b)
+      (setq result (cons (cons (car a) (car b)) result)
+	    a (cdr a)
+	    b (cdr b)))
+    (nreverse result)))
+
 (defun myfitnesspal-parse-file (file)
   "Parse a myfitnesspal FILE.
 The result is an alist element of meal data."
   (save-window-excursion
-    (find-file file)
+    (let ((buffer-to-scrape (find-buffer-visiting file)))
+      (if (bufferp buffer-to-scrape)
+	  (progn
+	    (set-buffer buffer-to-scrape)
+	    (revert-buffer t t))
+	(find-file file)))
     (save-excursion
       (goto-char (point-min))
       (re-search-forward "// </food/diary\\?date=\\(....\\)-\\(..\\)-\\(..\\)>")
@@ -39,31 +52,47 @@ The result is an alist element of meal data."
 	     (yesterday-day (string-to-number (match-string-no-properties 3)))
 	     (yesterday-time (encode-time 1 1 1 yesterday-day month year))
 	     (today-time (time-add yesterday-time (days-to-time 1)))
+	     (decoded-today (decode-time today-time))
+	     (year-number (nth 5 decoded-today))
+	     (month-number (nth 4 decoded-today))
+	     (day-number (nth 3 decoded-today))
+	     (column-labels-string (save-excursion
+				     (re-search-forward "Breakfast\\s-+\\(.+\\)$")
+				     (match-string-no-properties 1)))
+	     (column-labels (mapcar 'intern
+				    (split-string column-labels-string
+						  "\\s-+" t)))
 	     (meals nil))
 	(while (search-forward "Add Food" (point-max) t)
 	  (save-excursion
 	    (re-search-forward "^\t[0-9,\t ]+$" (point-max) t)
-	    (let* ((meal-data (convert-to-numbers 
+	    (let* ((meal-data (convert-to-numbers
 			       (split-string (match-string-no-properties 0) "\\s-+" t))))
 	      (re-search-backward "Breakfast\\|Lunch\\|Supper\\|Snacks")
-	      (push (cons (match-string-no-properties 0) meal-data)
-		    meals))))
+	      (let* ((meal-name (intern (match-string-no-properties 0))))
+		(push (cons meal-name (zip-cons-lists column-labels meal-data))
+		      meals)))))
 	(re-search-forward  "^Totals\\s-+\\([0-9,\t ]+\\)$")
-	(push (cons "Totals" (convert-to-numbers
-			      (split-string (match-string-no-properties 1) "\\s-+" t)))
+	(push (cons 'Totals (zip-cons-lists
+			     (convert-to-numbers
+			      (split-string (match-string-no-properties 1) "\\s-+" t))
+			     column-labels))
 	      meals)
-	(cons today-time meals)))))
+	(cons today-time (cons (list year-number month-number day-number)
+			       (nreverse meals)))))))
 
 (defvar myfitnesspal-accumulated-data-file "~/myfitnesspal-data-el"
   "File containing accumulated scrapes.")
 
 (defun myfitnesspal-read-accumulated-data ()
   "Read the accumulated data."
-  (save-window-excursion
-    (find-file myfitnesspal-accumulated-data-file)
-    (save-excursion
-      (goto-char (point-min))
-      (read (current-buffer)))))
+  (if (file-exists-p myfitnesspal-accumulated-data-file)
+      (save-window-excursion
+	(find-file myfitnesspal-accumulated-data-file)
+	(save-excursion
+	  (goto-char (point-min))
+	  (read (current-buffer))))
+    nil))
 
 (defun myfitnesspal-write-accumulated-data (data)
   "Write the accumulated data."
@@ -77,18 +106,20 @@ The result is an alist element of meal data."
       (insert ")\n")
       (basic-save-buffer))))
 
-(defun myfitnesspal-work-backwards ()
+(defun myfitnesspal-work-backwards (starting-back)
   "Work through a series of report pages."
+  (interactive "nStart how many days ago: ")
   (let ((accumulated (myfitnesspal-read-accumulated-data))
 	(page-file-name nil)
 	(another t)
-	(back 0))
+	(back starting-back))
     (while another
       (myfitnesspal-fetch-days-ago back)
-      (unless page-file-name
-	(setq page-file-name (read-file-name "File of saved web page: ")))
+      (setq page-file-name (read-file-name "File of saved web page: "
+					   nil page-file-name))
       (push (myfitnesspal-parse-file page-file-name) accumulated)
       (setq another (y-or-n-p "Fetch another page? ")
 	    back (1+ back)))
-    (myfitnesspal-write-accumulated-data accumulated)))
+    (myfitnesspal-write-accumulated-data accumulated)
+    (message "Finished %d days ago" back)))
     
