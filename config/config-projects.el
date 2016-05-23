caa6a8ace61a662a1dfbf1af8be26f5adf868b26
John Sturdy <john.sturdy@arm.com>
2016-05-23 13:26:48 +0100

    Stop FPGA project shells from preloading vendor software.
    To minimize confusion, I want to start each run from a plain base.
    
    Also, update directory names to suit changes to my Linux setup.

--- config/config-projects.el
+++ config/config-projects.el
@@ -1,5 +1,5 @@
 ;;;; Configuration for project-specific things
-;;; Time-stamp: <2016-03-17 14:37:56 johstu01>
+;;; Time-stamp: <2016-05-23 13:20:50 johstu01>
 
 ;; Copyright (C) 2007, 2008, 2009, 2010, 2012, 2013, 2014, 2015, 2016, John C. G. Sturdy
 
@@ -38,11 +38,13 @@
  (when (at-work)
   (make-shell-for-directory-if-present "/work/johstu01/build/trunk/work/src" "-src-")
   (make-shell-for-directory-if-present "/work/johstu01/build/trunk/work/src/arm.com/uniSched" "-uniSched-")
-  (make-shell-for-directory-if-present "/home/johstu01/fpga/farms" "-farms-")
-  (make-shell-for-directory-if-present "/home/johstu01/fpga/hapsfarm" "-hapsfarm-" 
-				       ". ~/.bash_profile; module load eda synopsys/xactors/2015.03")
-  (make-shell-for-directory-if-present "/home/johstu01/fpga/profpgafarm" "-profpgafarm-"
-				       ". ~/.bash_profile; module load eda prodesign/proFPGA/2015C")
+  (make-shell-for-directory-if-present "/nethome/johstu01/fpga/farms" "-farms-")
+  (make-shell-for-directory-if-present "/nethome/johstu01/fpga/hapsfarm" "-hapsfarm-" 
+				       ". ~/.bash_profile\n" ; ". ~/.bash_profile; module load eda synopsys/xactors/2015.03"
+				       )
+  (make-shell-for-directory-if-present "/nethome/johstu01/fpga/profpgafarm" "-profpgafarm-"
+				       ". ~/.bash_profile\n" ;". ~/.bash_profile; module load eda prodesign/proFPGA/2015C"
+				       )
   ;; (make-shell-for-directory-if-present "/work/johstu01/build/trunk/work/src/arm.com/gocommons" "-gocommons-")
   ;; (make-shell-for-directory-if-present "/work/johstu01/build/trunk/work/src/arm.com/uniSched/overseer" "-monit-")
   ;; (make-shell-for-directory-if-present "/work/johstu01/build/trunk/work/src/arm.com/uniSched/dryrun" "-dripfeed-")
