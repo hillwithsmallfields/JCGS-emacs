1ca09199289b18832162fb003903b20e1df234fd
John Sturdy <jcg.sturdy@gmail.com>
2016-07-18 21:27:55 +0100

    Got it mostly working.

--- webstuff/journal-html-to-org.el
+++ webstuff/journal-html-to-org.el
@@ -1,5 +1,5 @@
 ;;;; journal-html-to-org.el --- convert my old HTML journals to org-mode
-;;; Time-stamp: <2016-07-17 21:59:23 jcgs>
+;;; Time-stamp: <2016-07-18 21:26:42 jcgs>
 
 (require 'journal)
 (require 'replace-regexp-list)
@@ -21,23 +21,60 @@
 	       (month (cdr (assoc month-name journal-monthname-alist)))
 	       (day (string-to-number (match-string-no-properties 3 html-date)))
 	       (encoded (encode-time 0 0 0 day month year)))
-	  (list month day year)))))
+	  (list year month day)))))
+
+(org-add-link-type "journal-people" 'journal-people-open)
+(add-hook 'org-store-link-functions 'journal-people-store-link)
+
+(defun journal-people-open (path)
+  "Open the people entry at PATH."
+  ;; todo: complete this
+  )
+
+(defun journal-people-store-link ()
+  "Store a people link."
+  (let ((link "")			; todo: complete
+	(description ""))		; todo: complete
+    (org-store-link-props
+     :type "journal-people"
+     :link link
+     :description description)))
+
+(org-add-link-type "journal-date" 'journal-date-open)
+(add-hook 'org-store-link-functions 'journal-date-store-link)
+
+(defun journal-date-open (path)
+  "Open the date entry at PATH."
+  ;; todo: complete this
+  )
+
+(defun journal-date-store-link ()
+  "Store a date link."
+  (let ((link "")			; todo: complete
+	(description ""))		; todo: complete
+    (org-store-link-props
+     :type "journal-date"
+     :link link
+     :description description)))
 
 (defvar journal-html-to-org-edits
-  '(("<h2>.+\\[\\([-0-9a-z]+\\).+</h2>" . "*** \\,(journal-html-to-org-date \\1)")
-    ("<a href=\"\\([^\"]+\\)\">\\([^<]+\\)</a>" . "[people:\\1|\\2]")
-    ("</?p>" . "")
+  '(("<a href=\"../../people/\\([^\"]+\\)\">\\([^<]+\\)</a>" . "[[journal-people:\\1][\\2]]")
+    ("<a href=\"\\([0-9][0-9]-[0-9][0-9]\\).html#\\([0-9][0-9]\\)\">\\([^<]+\\)</a>" . "[[journal-date:\\1-\\2][\\3]]")
+    ("<a href=\"../[0-9][0-9][0-9][0-9]/\\([0-9][0-9]-[0-9][0-9]\\).html#\\([0-9][0-9]\\)\">\\([^<]+\\)</a>" . "[[journal-date:\\1-\\2][\\3]]")
     )
   "Edits to make to convert a journal file.")
 
 (defun journal-html-get-file-entries (file)
   "Return the journal entries in FILE."
   (save-excursion
-    (find-file file)
+    (switch-to-buffer (get-buffer-create " *Conversion*"))
+    (erase-buffer)
+    (insert-file-contents file)
+    ;; (replace-regexp-alist journal-html-to-org-edits)
     (goto-char (point-max))
     (unless (and (re-search-backward "^.*<hr>" (point-min) t)
 		 (> (point)
-		    (- (point-max) 256)))
+		    (- (point-max) 400)))
       (error "Could not find realistic end of journal"))
     (let ((entry-end (point))
 	  (entries nil))
@@ -66,6 +103,7 @@
 	  (beginning-of-line 1)
 	  (setq entry-end (point))
 	  (message "resuming search from %d" (point))))
+      (bury-buffer)
       entries)))
 
 (defun journal-html-to-org-test (input-file)
@@ -77,14 +115,17 @@ Argument INPUT-FILE is the input file."
       (dolist (entry entries)
 	(princ (format "\n\n%S:\n" (car entry)))
 	(dolist (para (cdr entry))
-	  (princ (format "    %s\n\n" para)))))))
+	  (princ (format "    %s\n\n" para)))))
+    (find-file "/tmp/journal.org")
+    (journal-html-to-org-add-entries entries)))
 
 (defun journal-html-to-org-add-entries (entries)
   "Add ENTRIES to the current file."
   (dolist (entry entries)
-    (apply work-log-open-date (car entry))
+    (apply 'work-log-open-date (car entry))
+    (insert "\n")
     (dolist (para (cdr entry))
-      (insert "    " para "\n\n"))))
+      (insert "   " para "\n\n"))))
 
 (defun journal-html-to-org-add-file (org-file html-file)
   "Into ORG-FILE add HTML-FILE.
@@ -93,7 +134,9 @@ It always adds the new file at the end."
   (find-file org-file)
   (goto-char (point-max))
   (journal-html-to-org-add-entries
-   (journal-html-get-file-entries html-file)))
+   (journal-html-get-file-entries html-file))
+  (replace-regexp-alist journal-html-to-org-edits)
+  (fill-individual-paragraphs (point-min) (point-max)))
 
 (defun file-name-last-directory (directory)
   "Return the last directory part of DIRECTORY."
@@ -105,7 +148,8 @@ It always adds the new file at the end."
   "Make up an org version of the journal files in DIRECTORY."
   (interactive "DConvert journal in directory: ")
   (let* ((dir-dirname (file-name-last-directory directory))
-	 (org-filename (expand-file-name (concat dir-dirname ".org") directory))
+	 (output-directory (expand-file-name ".." directory))
+	 (org-filename (expand-file-name (concat dir-dirname ".org") output-directory))
 	 (html-files (directory-files directory t "[0-9][0-9]-[0-9][0-9]\\.html")))
     (find-file org-filename)
     (erase-buffer)
@@ -113,6 +157,15 @@ It always adds the new file at the end."
      (apply 'concatenate 'list
 	    (mapcar 'journal-html-get-file-entries
 		    html-files)))
+    (replace-regexp-alist journal-html-to-org-edits)
+    (fill-individual-paragraphs (point-min) (point-max))
     (basic-save-buffer)))
+
+(defun journal-html-to-org-subdirectories (directory)
+  "Make up org versions of subdirectories of DIRECTORY."
+  (interactive "DConvert journal in subdirectories of: ")
+  (mapcar 'journal-html-to-org-directory
+	  (directory-files directory t
+			   "[0-9]\\{4\\}")))
 
 (provide 'journal-html-to-org)
