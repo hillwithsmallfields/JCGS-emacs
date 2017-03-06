6b4c153fb438dc190fac0a467bc41f40364d371a
John Sturdy <john.sturdy@arm.com>
2017-03-06 11:07:38 +0000

    Add function to show where point is in the tree, from emacswiki

--- config/config-from-emacswiki.el
+++ config/config-from-emacswiki.el
@@ -1,4 +1,24 @@
-;; Time-stamp: <2011-02-25 11:46:08 johnstu>
+;; Time-stamp: <2017-03-01 17:10:25 johstu01>
 
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; from https://www.emacswiki.org/emacs/NxmlMode
+
+(defun nxml-where ()
+  "Display the hierarchy of XML elements the point is on as a path."
+  (interactive)
+  (let ((path nil))
+    (save-excursion
+      (save-restriction
+	(widen)
+	(while (and (< (point-min) (point)) ;; Doesn't error if point is at beginning of buffer
+		    (condition-case nil
+			(progn
+			  (nxml-backward-up-element) ; always returns nil
+			  t)
+		      (error nil)))
+	  (setq path (cons (xmltok-start-tag-local-name) path)))
+	(if (called-interactively-p t)
+	    (message "/%s" (mapconcat 'identity path "/"))
+	  (format "/%s" (mapconcat 'identity path "/")))))))
