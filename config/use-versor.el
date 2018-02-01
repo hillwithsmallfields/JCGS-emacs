56d0d9e971600b26fca821e04e0c50dc68ddb0ef
John Sturdy <john.sturdy@grapeshot.com>
2018-02-01 17:02:41 +0000

    Re-enable joystick

--- config/use-versor.el
+++ config/use-versor.el
@@ -1,5 +1,5 @@
 ;;;; find, load and configure versor
-;;; Time-stamp: <2016-04-21 13:50:43 johstu01>
+;;; Time-stamp: <2018-01-04 17:16:01 jcgs>
 
 (setq joystick-graphical nil)
 
@@ -29,7 +29,7 @@
 			   'meta
 			   'research
 			   'modal
-			   ;; 'joystick
+			   'joystick
 			   ;; 'local
 			   'menu
 			   'quiet-underlying
