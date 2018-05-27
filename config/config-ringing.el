543bc0f77dbd6ad02f32b3b029b4a0b6aa128390
John Sturdy <jcg.sturdy@gmail.com>
2018-05-27 21:18:49 +0100

    use symbol for require

--- config/config-ringing.el
+++ config/config-ringing.el
@@ -1,5 +1,5 @@
 ;;;; Config for emacs-ringing
-;; Time-stamp: <2018-04-27 16:38:02 jcgs>
+;; Time-stamp: <2018-04-27 21:37:31 jcgs>
 ;; I'm resuming using this after a long gap, and have probably lost my old configuration for it
 
 (add-to-list 'load-path (substitute-in-file-name "$OPEN_PROJECTS/emacs-ringing/lisp"))
