a72b5f4e72b2c2184a40b81cee9ebd4b391872dd
John Sturdy <john.sturdy@oracle.com>
2019-06-10 11:07:20 +0100

    Move shell command recorder out of line from my main emacs config
    It's now in my org-mode extensions repo.

--- config/config-distribution.el
+++ config/config-distribution.el
@@ -1,5 +1,5 @@
 ;;;; Configuration for things included in the emacs distribution
-;;; Time-stamp: <2019-04-24 10:53:27 jcgs>
+;;; Time-stamp: <2019-06-10 10:57:25 jcgs>
 
 ;; Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, John C. G. Sturdy
 
@@ -501,196 +501,6 @@ The nearest FILE is used."
   (let ((inhibit-read-only t))
     (comint-send-input)))
 
-(defvar jcgs/shell-mode-recorded-commands nil
-  "For deduplication.
-Not persistent between sessions, and reset each day.")
-
-(make-variable-buffer-local 'jcgs/shell-mode-recorded-commands)
-
-(defconst jcgs/shell-command-record-regexp
-  "^ +\\([^:]+\\):\\([^$]+\\)\\$ \\(.+\\)$")
-
-(defun jcgs/shell-command-records-get-buffer ()
-  "Get the buffer of the current line, and put it on the kill ring."
-  (interactive)
-  (save-excursion
-    (beginning-of-line 1)
-    (if (looking-at jcgs/shell-command-record-regexp)
-        (kill-new (match-string-no-properties 1))
-      (error "Not on a shell history line"))))
-
-(defun jcgs/shell-command-records-get-command ()
-  "Get the command of the current line, and put it on the kill ring.."
-  (interactive)
-  (save-excursion
-    (beginning-of-line 1)
-    (if (looking-at jcgs/shell-command-record-regexp)
-        (kill-new (match-string-no-properties 3))
-      (error "Not on a shell history line"))))
-
-(defun jcgs/shell-command-records-get-directory ()
-  "Get the directory of the current line, and put it on the kill ring.."
-  (interactive)
-  (save-excursion
-    (beginning-of-line 1)
-    (if (looking-at jcgs/shell-command-record-regexp)
-        (kill-new (match-string-no-properties 2))
-      (error "Not on a shell history line"))))
-
-(defun jcgs/shell-command-records-set-buffer-other-window ()
-  "Select the buffer of the current line, in another window."
-  (interactive)
-  (switch-to-buffer-other-window
-   (save-excursion
-     (beginning-of-line 1)
-     (if (looking-at jcgs/shell-command-record-regexp)
-         (match-string-no-properties 1)
-       (error "Not on a shell history line")))))
-
-(defun jcgs/shell-command-records-insert-command-other-window ()
-  "Insert the command on this line into the other window."
-  (interactive)
-  (let ((command (save-excursion
-                   (beginning-of-line 1)
-                   (if (looking-at jcgs/shell-command-record-regexp)
-                       (match-string-no-properties 3)
-                     (error "Not on a shell history line")))))
-    (other-window 1)
-    (insert command)))
-
-(defun jcgs/shell-command-records-run-in-nearest-shell (&optional no-confirm)
-  "Run the command on this line, in the most recent shell buffer.
-Ask for confirmation of the buffer, except with prefix arg NO-CONFIRM."
-  (interactive "P")
-  (let* ((command (save-excursion
-                    (beginning-of-line 1)
-                    (if (looking-at jcgs/shell-command-record-regexp)
-                        (match-string-no-properties 3)
-                      (error "Not on a shell history line"))))
-         (shell-buffer (catch 'got-one
-                         (dolist (buffer (buffer-list))
-                           (set-buffer buffer)
-                           (when (eq major-mode 'shell-mode)
-                             (throw 'got-one buffer)))
-                         nil)))
-    (when (and shell-buffer (or no-confirm (y-or-n-p (format "Run %s in %s? " command shell-buffer))))
-      (switch-to-buffer shell-buffer)
-      (comint-send-string (get-buffer-process shell-buffer) (concat command "\n")))))
-
-(defun jcgs/shell-command-records-run-in-same-shell (&optional no-confirm)
-  "Run the command on this line, in the same shell it was originally run in.
-Ask for confirmation of the buffer, except with prefix arg NO-CONFIRM."
-  (interactive "P")
-  (let* ((buffer-directory-command (save-excursion
-                                     (beginning-of-line 1)
-                                     (if (looking-at jcgs/shell-command-record-regexp)
-                                         (list
-                                          (match-string-no-properties 1)
-                                          (match-string-no-properties 2)
-                                          (match-string-no-properties 3))
-                                       (error "Not on a shell history line"))))
-         (shell-buffer (nth 0 buffer-directory-command))
-         (directory (nth 1 buffer-directory-command))
-         (command (nth 2 buffer-directory-command)))
-    (when (and shell-buffer (or no-confirm (y-or-n-p (format "Run %s in %s? (dir %s)" command shell-buffer directory))))
-      (switch-to-buffer shell-buffer)
-      (unless (equal directory default-directory)
-        (comint-send-string (get-buffer-process shell-buffer)
-                            (concat "cd " directory "\n")))
-      (comint-send-string (get-buffer-process shell-buffer) (concat command "\n")))))
-
-(defun jcgs/shell-command-records-run-in-same-shell-with-gdb (&optional no-confirm)
-  "Run the command on this line with gdb, in the same shell it was originally run in.
-Ask for confirmation of the buffer, except with prefix arg NO-CONFIRM."
-  (interactive "P")
-  (let* ((buffer-directory-command (save-excursion
-                                     (beginning-of-line 1)
-                                     (if (looking-at
-                                          jcgs/shell-command-record-regexp)
-                                         (list
-                                          (match-string-no-properties 1)
-                                          (match-string-no-properties 2)
-                                          (match-string-no-properties 3))
-                                       (error "Not on a shell history line"))))
-         (shell-buffer (nth 0 buffer-directory-command))
-         (directory (nth 1 buffer-directory-command))
-         (command-line-parts (split-string (nth 2 buffer-directory-command)))
-         (command (car command-line-parts))
-         (command-args (mapconcat 'identity
-                                  (cons "run" (cdr command-line-parts))
-                                  " ")))
-    (when (and shell-buffer (or no-confirm
-                                (y-or-n-p (format "Run %s in %s? (dir %s)"
-                                                  command
-                                                  shell-buffer
-                                                  directory))))
-      (switch-to-buffer shell-buffer)
-      (unless (equal directory default-directory)
-        (comint-send-string (get-buffer-process shell-buffer)
-                            (concat "cd " directory "\n")))
-      (kill-new command-args)
-      (message "run command %S is on the kill ring" command-args)
-      (comint-send-string (get-buffer-process shell-buffer)
-                          (concat "gdb " command "\n")))))
-
-(defvar jcgs/shell-command-records-mode-map
-  (let ((map (make-keymap)))
-    (suppress-keymap map)
-    (define-key map "b" 'jcgs/shell-command-records-get-buffer)
-    (define-key map "c" 'jcgs/shell-command-records-get-command)
-    (define-key map "d" 'jcgs/shell-command-records-get-directory)
-    (define-key map "g" 'jcgs/shell-command-records-run-in-same-shell-with-gdb)
-    (define-key map "B" 'jcgs/shell-command-records-set-buffer-other-window)
-    (define-key map "C" 'jcgs/shell-command-records-insert-command-other-window)
-    (define-key map "r" 'jcgs/shell-command-records-run-in-nearest-shell)
-    (define-key map (kbd "RET") 'jcgs/shell-command-records-run-in-same-shell)
-    (define-key map "n" 'next-line)
-    (define-key map "p" 'previous-line)
-    (define-key map "u" 'outline-up-heading)
-    map))
-
-(define-derived-mode jcgs/shell-command-records-mode jcgs/org-journal-mode "Shell command records"
-  "Major mode for an accumulated list of shell commands.")
-
-(defun jcgs/shell-mode-record-command-in-journal (shell-command)
-  "Record SHELL-COMMAND in my journal file."
-  (when (and (boundp 'jcgs/shell-mode-accumulated-command-history-file)
-	     (stringp jcgs/shell-mode-accumulated-command-history-file)
-	     (file-writable-p jcgs/shell-mode-accumulated-command-history-file)
-	     (not (string-match "^!\\(!\\|[-0-9]+\\)$" shell-command)))
-    (set-text-properties 0 (length shell-command)
-			 nil shell-command)
-    (let ((cwd default-directory)
-	  (buffer (buffer-name)))
-      (save-window-excursion
-	(find-file jcgs/shell-mode-accumulated-command-history-file)
-	(goto-char (point-max))
-	(when (let ((date (decode-time)))
-		(jcgs/org-journal-open-date (nth 5 date) (nth 4 date) (nth 3 date) t
-                                            'jcgs/shell-command-records-mode))
-	  (setq jcgs/shell-mode-recorded-commands nil))
-	(goto-char (point-max))
-	(dolist (command-line (split-string shell-command "\n" t))
-	  ;; todo: fix this filtering, it doesn't seem to be working
-	  ;; todo: filter out history commands that don't alter anything
-	  ;; todo: filter out "history" commands
-	  (unless (member command-line jcgs/shell-mode-recorded-commands)
-	    (push command-line jcgs/shell-mode-recorded-commands)
-	    ;; (message "Got shell command %S to record as happening in buffer %S with default directory %s." command-line buffer cwd)
-	    (let ((was-cd (string-match "^cd\\(?:\\s-+\\(.+\\)\\)?" command-line)))
-	      (when was-cd
-		(let ((cd-arg (match-string 1 command-line)))
-		  ;; (message "old cwd %S" cwd)
-		  (setq cwd (if cd-arg
-				(expand-file-name cd-arg cwd) ; todo: this area is not behaving as I expected, cwd is already one too high
-			      (getenv "HOME")))
-		  ;; (message "Command was cd, with arg %S; cwd now %S" cd-arg cwd)
-		  ))))
-	  (delete-blank-lines)
-	  (insert "    " buffer ":" cwd "$ " command-line)
-	  (basic-save-buffer)
-	  (bury-buffer))))))
-
 (defun jcgs/shell-mode-interrupt-subjob ()
   "Getting around a problem with it complaining that the text is
 read-only (although I don't think I'd changed anything related)
@@ -713,6 +523,8 @@ read-only (although I don't think I'd changed anything related)
     (if ticket
         (insert (match-string 0))
       (error "Could not find anything that looked like a JIRA ticket"))))
+
+(require 'org-shell-command-records)
 
 (defun jcgs/shell-mode-setup ()
   "Set shell mode up the way I want it."
