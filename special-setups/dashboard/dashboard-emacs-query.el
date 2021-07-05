cd688aef428cdb7a127edd18fcc0f1208eb4965e
John Sturdy <jcg.sturdy@gmail.com>
2021-07-05 20:25:34 +0100

    Load specific versions of packages, and set up some orq-ql views

--- special-setups/dashboard/dashboard-emacs-query.el
+++ special-setups/dashboard/dashboard-emacs-query.el
@@ -27,12 +27,33 @@
 (require 'cl)
 
 (add-to-list 'load-path (substitute-in-file-name "$MY_PROJECTS//github.com/hillwithsmallfields/JCGS-org-mode/lisp/"))
-
+(dolist (dir '("~/emacs-packages/org-ql-20191105.2141/"
+               "~/emacs-packages/dash-20191109.1327/"
+               "~/emacs-packages/dash-functional-20191109.1327/"
+               "~/emacs-packages/ts-20191010.210/"
+               "~/emacs-packages/s-20180406.808/"
+               "~/emacs-packages/peg-20150708.641"
+               "~/emacs-packages/org-super-agenda-20190925.958"
+               "~/emacs-packages/ht-20190924.704/"
+               "~/emacs-packages/ov-20150312.528/"))
+  (add-to-list 'load-path (expand-file-name dir)))
+(message "loading org-ql")
+;; (load-library "org-ql")
+(require 'org-ql)
+(require 'org-ql-view)
+(message "loaded org-ql")
+(setq package-user-dir (substitute-in-file-name "$EHOME/emacs-packages"))
 (load-file (substitute-in-file-name "$MY_ELISP/information-management/metoffice.el"))
-(load-file (substitute-in-file-name "$MY_ELISP/config/config-org-mode.el"))
 (load-file (substitute-in-file-name "$MY_PROJECTS/JCGS-org-mode/lisp/org-ql-to-json.el"))
+(message "(boundp 'org-ql-views) = %s" (boundp 'org-ql-views))
+(load-file (substitute-in-file-name "$MY_ELISP/config/config-org-mode.el"))
 
-(jcgs/org-ql-views-to-json (substitute-in-file-name "$COMMON/var/views.json") '("Current"))
+(jcgs/org-ql-views-to-json (substitute-in-file-name "$COMMON/var/views.json")
+                           '("Current"
+                             "Supermarket"
+                             "Physical making"
+                             "Programming"
+                             "Mending"))
 
 (provide 'dashboard-emacs-query)
 ;;; dashboard-emacs-query.el ends here
