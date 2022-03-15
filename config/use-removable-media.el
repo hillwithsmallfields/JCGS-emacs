bd66da225e50416e2e713c65be2427c4d2f1d73e
John Sturdy <jcg.sturdy@gmail.com>
2022-03-15 10:33:17 +0000

    suppress more old stuff

--- config/use-removable-media.el
+++ config/use-removable-media.el
@@ -1,18 +1,18 @@
 ;;;; use-removable-media.el -- look for USB keys etc
-;;; Time-stamp: <2020-11-11 20:56:08 jcgs>
+;;; Time-stamp: <2022-03-15 10:33:38 jcgs>
 
-(require 'jcgs-use-package)
+;; (require 'jcgs-use-package)
 
-(jcgs/use-package 'removable-media
-	     (expand-file-name "file-handling" user-emacs-directory)
-	     "http://www.cb1.com/~john/computing/emacs/file-handling/removable-media.el"
-	     ((expand-file-name "webstuff" user-emacs-directory)
-	      (mount-removable-top-level-directory "removable-media" nil t)
-	      (html-journal-helper-mode "journal" nil t)
-	      ("/personal/journal/dates/[/0-9]+/.+\\.html" .
-	       html-journal-helper-mode)))
+;; (jcgs/use-package 'removable-media
+;; 	     (expand-file-name "file-handling" user-emacs-directory)
+;; 	     "http://www.cb1.com/~john/computing/emacs/file-handling/removable-media.el"
+;; 	     ((expand-file-name "webstuff" user-emacs-directory)
+;; 	      (mount-removable-top-level-directory "removable-media" nil t)
+;; 	      (html-journal-helper-mode "journal" nil t)
+;; 	      ("/personal/journal/dates/[/0-9]+/.+\\.html" .
+;; 	       html-journal-helper-mode)))
 
-(mapcar 'mount-removable-top-level-directory
-	'("watch" "personal" "usb3" "usb4"))
+;; (mapcar 'mount-removable-top-level-directory
+;; 	'("watch" "personal" "usb3" "usb4"))
 
 ;;; end of use-removable-media.el
