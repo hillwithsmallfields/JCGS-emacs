1dda35f3eb23198d022904d2c03f361124d2825b
John Sturdy <jcg.sturdy@gmail.com>
2022-03-15 10:29:40 +0000

    don't load auctex if I don't have it installed
    Needs further work to install it properly the modern way.

--- config/use-auctex.el
+++ config/use-auctex.el
@@ -1,48 +1,51 @@
 ;;;; find, load and configure auctex
-;;; Time-stamp: <2020-11-11 20:56:07 jcgs>
+;;; Time-stamp: <2022-03-15 10:29:12 jcgs>
 
-(require 'jcgs-use-package)
+(let ((jcgs-auctex-dir "$GATHERED/emacs/auctex/installed/"))
+  (when (file-directory-p jcgs-auctex-dir)
 
-(fset 'tex-mode nil)
-(fset 'latex-mode nil)
+    (require 'jcgs-use-package)
 
-(add-to-list 'load-path (substitute-in-file-name "$GATHERED/emacs/auctex/installed/"))
-(load-file "$GATHERED/emacs/auctex/installed/tex-site.el")
+    (fset 'tex-mode nil)
+    (fset 'latex-mode nil)
 
-(jcgs/use-package tex-site
-	     "$GATHERED/emacs/auctex/installed"
-	     nil
-	     (("\\.tex$" . tex-mode)
-	      "$GATHERED/emacs/auctex/installed/auctex"
-	      (expand-file-name "graphics" user-emacs-directory)
-	      (view-dvi-file "dvi-view" "View DVI-FILE in an Emacs buffer." t))
-	     (setq tex-dvi-view-command "/usr/bin/xdvi"
-		   tex-dvi-print-command "dvips"
-		   TeX-auto-save t
-		   TeX-parse-self t
-		   TeX-newline-function 'reindent-then-newline-and-indent
-		   TeX-auto-untabify t
-		   ;; TeX-auto-local "auto/"
-		   TeX-lisp-directory (substitute-in-file-name "$GATHERED/emacs/auctex/installed/auctex")
-		   find-function-regexp
-		   ;; include TeX-defun
-		   "^\\s-*(\\(TeX-\\)?\\(def\\(ine-skeleton\\|ine-generic-mode\\|ine-derived-mode\\|\
+    (add-to-list 'load-path (substitute-in-file-name jcgs-auctex-dir))
+    (load-file "$GATHERED/emacs/auctex/installed/tex-site.el")
+
+    (jcgs/use-package tex-site
+	              "$GATHERED/emacs/auctex/installed"
+	              nil
+	              (("\\.tex$" . tex-mode)
+	               "$GATHERED/emacs/auctex/installed/auctex"
+	               (expand-file-name "graphics" user-emacs-directory)
+	               (view-dvi-file "dvi-view" "View DVI-FILE in an Emacs buffer." t))
+	              (setq tex-dvi-view-command "/usr/bin/xdvi"
+		            tex-dvi-print-command "dvips"
+		            TeX-auto-save t
+		            TeX-parse-self t
+		            TeX-newline-function 'reindent-then-newline-and-indent
+		            TeX-auto-untabify t
+		            ;; TeX-auto-local "auto/"
+		            TeX-lisp-directory (substitute-in-file-name "$GATHERED/emacs/auctex/installed/auctex")
+		            find-function-regexp
+		            ;; include TeX-defun
+		            "^\\s-*(\\(TeX-\\)?\\(def\\(ine-skeleton\\|ine-generic-mode\\|ine-derived-mode\\|\
 \[^cgv\W]\\w+\\*?\\)\\|define-minor-mode\
 \\|easy-mmode-define-global-mode\\)\\(\\s-\\|\n\\)+'?\
 %s\\(\\s-\\|$\\|\(\\|\)\\)")
-	     (add-hook 'LaTeX-mode-hook
-		       '(lambda ()
-			  (TeX-fold-mode 1)
-			  (set-fill-column 60)))
-	     (load-library "tex.el")
-	     (load-library "latex.el"))
+	              (add-hook 'LaTeX-mode-hook
+		                '(lambda ()
+			           (TeX-fold-mode 1)
+			           (set-fill-column 60)))
+	              (load-library "tex.el")
+	              (load-library "latex.el"))
 
-(setq-default TeX-master nil)
-(put 'TeX-master 'safe-local-variable
-     (function
-      (lambda (file)
-	(message "Checking \"%s\" for safety as TeX-master" file)
-	(or (eq file t)
-	    (not (string-match "/" file))))))
+    (setq-default TeX-master nil)
+    (put 'TeX-master 'safe-local-variable
+         (function
+          (lambda (file)
+	    (message "Checking \"%s\" for safety as TeX-master" file)
+	    (or (eq file t)
+	        (not (string-match "/" file))))))))
 
 ;;; end of use-auctex.el
