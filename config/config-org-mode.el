32872f8ac4c99099050d2e2dd450cee3fa1a68ea
John Sturdy <jcg.sturdy@gmail.com>
2025-07-18 15:18:30 +0100

    add boilerplate for journalling

--- config/config-org-mode.el
+++ config/config-org-mode.el
@@ -1,5 +1,5 @@
 ;;; config-org-mode.el --- set up JCGS' org mode
-;;; Time-stamp: <2025-01-10 08:36:16 jcgs>
+;;; Time-stamp: <2025-07-18 15:12:22 jcgs>
 
 (defconst jcgs-org-supporting-libraries
   '(("org-ql" . "github.com/alphapapa/org-ql")
@@ -63,7 +63,7 @@ Done when I gave up on the Emacs package manager for now.")
       org-clock-in-switch-to-state "CURRENT"
       org-use-fast-todo-selection nil
       org-id-method 'org
-      org-adapt-indentation t
+      oorg-adapt-indentation t
       org-id-track-globally t
       org-id-locations-file (substitute-in-file-name "$ORG/org-id-locations")
       org-id-locations-file-relative t
@@ -120,6 +120,12 @@ Done when I gave up on the Emacs package manager for now.")
       org-enforce-todo-checkbox-dependencies t
       org-todo-state-tags-triggers '((done ("soon" . nil) ("urgent" . nil) ("today" . nil)))
       org-M-RET-may-split-line nil
+      jcgs/org-journal-day-boilerplate '(("incoming.journal" . " [/]
+
+    - [ ] prayertime
+    - [ ] strength exercises
+    - [ ] Albanian learning
+    - [ ] reading\n")
       )
 
 (defvar jcgs/org-ssid-tag-alist
